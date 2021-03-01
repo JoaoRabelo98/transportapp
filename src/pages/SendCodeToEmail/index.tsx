863e1ff648bcab4eccf48f88ee494426fc44e7a1
joaor <joaor@DESKTOP-66CCHKI>
2021-03-01 00:06:52 -0300

    feat: finalize send code to e-mail

--- src/pages/SendCodeToEmail/index.tsx
+++ src/pages/SendCodeToEmail/index.tsx
@@ -1,47 +1,42 @@
 import React from 'react';
-import { KeyboardAvoidingView, ScrollView, Platform, Image } from 'react-native';
+import { Image } from 'react-native';
 
 import { Container, Title, SubTitle, MainContent, ViewButton } from './styles';
 import Button from '../../components/Button';
 import padLockImg from '../../assets/PadLock.png';
 import colors from '../../styles/colors';
 
-const Login: React.FC = () => {
+const ModalSendCodeToEmail: React.FC = () => {
     return (
-        <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-            enabled
-        >
-            <ScrollView
-                contentContainerStyle={{ flex: 1 }}
-                keyboardShouldPersistTaps="handled"
+        <Container>
+            <MainContent
+                style={{
+                    shadowColor: colors.shadow,
+                    shadowOffset: { width: 1, height: 1 },
+                    shadowOpacity: 0.5,
+                    shadowRadius: 2,
+                    elevation: 170,
+                }}
             >
-                <Container>
-                    <MainContent
-                        style={{
-                            shadowColor: colors.shadow,
-                            shadowOffset: { width: 1, height: 1 },
-                            shadowOpacity: 0.5,
-                            shadowRadius: 2,
-                            elevation: 150,
+                <Image source={padLockImg} />
+                <Title>Enviamos um código para o seu email</Title>
+                <SubTitle>
+                    Em breve, você receberá um e-mail com um código para configurar
+                    uma nova senha.
+                </SubTitle>
+
+                <ViewButton>
+                    <Button
+                        onPress={() => {
+                            console.log('confirmado');
                         }}
                     >
-                        <Image source={padLockImg} />
-                        <Title>Enviamos um código para o seu email</Title>
-                        <SubTitle>
-                            Em breve, você receberá um e-mail com um código para
-                            configurar uma nova senha.
-                        </SubTitle>
-
-                        <ViewButton>
-                            <Button onPress={() => {}}>Confirmar</Button>
-                        </ViewButton>
-                    </MainContent>
-                </Container>
-            </ScrollView>
-        </KeyboardAvoidingView>
+                        Confirmar
+                    </Button>
+                </ViewButton>
+            </MainContent>
+        </Container>
     );
 };
 
-export default Login;
+export default ModalSendCodeToEmail;
