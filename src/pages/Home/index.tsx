a59b8b80373fca98c1f147d37fd958be7431a313
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-03-02 21:37:00 -0300

    feat: add BackgroundImage on HomeScreen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import { KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
 
-import { Text } from 'react-native';
+import {
+    Container,
+    Header,
+    BackgroundImageStyled,
+    DeliverymanImageStyled,
+} from './styles';
 
-import { Container } from './styles';
+import BackgroundImage1 from '../../assets/Home/BackgroundImage1.png';
+import DeliverymanImage from '../../assets/Home/BackgroundImage2.png';
 
 const Home: React.FC = () => {
     return (
-        <Container>
-            <Text>Hello World</Text>
-        </Container>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            enabled
+        >
+            <ScrollView
+                contentContainerStyle={{ flex: 1 }}
+                keyboardShouldPersistTaps="handled"
+            >
+                <Container>
+                    <Header></Header>
+                    <BackgroundImageStyled source={BackgroundImage1} />
+                    <DeliverymanImageStyled source={DeliverymanImage} />
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,31 @@
 import styled from 'styled-components/native';
+import colors from '../../styles/colors';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    flex: 1;
+    background: ${colors.primary};
+    /* background-color: #000; */
+    width: 100%;
+`;
+
+export const Header = styled.View`
+    padding-top: 113px;
+    margin-top: 0;
+    width: 100%;
+    height: 94px;
+    position: relative;
+    background: ${colors.button};
+`;
+
+export const BackgroundImageStyled = styled.Image`
+    width: 100%;
+    margin-top: -63px;
+    height: 308px;
+`;
+
+export const DeliverymanImageStyled = styled.Image`
+    margin-top: -172px;
+    margin-left: 18%;
+    width: 181px;
+    height: 172px;
+`;
